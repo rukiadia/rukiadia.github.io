90ec7de0cb93d59b718c6eb8339d6d61c03a0a67
Sho Yamada <sho.yamada@omicale.com>
2020-05-21 18:41:25 +0900

    デプロイ向けの設定を追加

--- next_blog/next.config.js
+++ next_blog/next.config.js
@@ -4,4 +4,5 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    assetPrefix: process.env.NODE_ENV === "production" ? "/next_blog" : ""
 };
