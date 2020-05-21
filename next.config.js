2afe73d3e6169c069c913720c77f3bf343294f6c
Sho Yamada <sho.yamada@omicale.com>
2020-05-21 18:56:50 +0900

    色々ややこしいので、ルートディレクトリに置く構造に戻した。

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ const path = require('path');
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
-    },
-    assetPrefix: process.env.NODE_ENV === "production" ? "/next_blog" : ""
+    }
 };
